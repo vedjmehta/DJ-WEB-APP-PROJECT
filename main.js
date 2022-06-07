c0e01f5fe2f8cfd42ee8fa77bcc927af7ab99367
vedjmehta <86235204+vedjmehta@users.noreply.github.com>
2022-06-07 14:20:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 song_1 = "";
 song_2 = "";
+leftWristX = 0;
+ leftWristY = 0;
+ rightWristX = 0;
+ rightWristY = 0;
 
 function preload() {
 
@@ -16,11 +20,31 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw() {
 
     image(video,0,0,600,500);
 }
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrsit Y =" + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X =" + rightWristX + "Right Wrist Y =" + rightWristY);
+    }
+  }
+ 
+  function modelLoaded() {
+      console.log("Pose Net has been intialized");
+  }
+ 
 
 
