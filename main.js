4b75303380e1c3edef041536ea6dc707bf5867d7
vedjmehta <86235204+vedjmehta@users.noreply.github.com>
2022-06-10 10:54:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristX = 0;
  leftWristY = 0;
  rightWristX = 0;
  rightWristY = 0;
+ scoreLeftWrist = 0;
 
 function preload() {
 
@@ -16,7 +17,7 @@ function preload() {
 function setup() {
 
     canvas = createCanvas(600,500);
-    canvas.position(375,200);
+    canvas.position(375,250);
 
     video = createCapture(VIDEO);
     video.hide();
@@ -28,10 +29,25 @@ function setup() {
 function draw() {
 
     image(video,0,0,600,500);
+
+    fill("#ff0000");
+    stroke("#000000");
+ 
+     if(scoreLeftWrist > 0.2) {
+
+    circle(leftWristX,leftWristY,20);
+    InNumberLeftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberLeftWristY);
+    song = "music.mp3";
+    document.getElementById("song").innerHTML = "Song Playing =" ;
+    song.play(song_2);
+     }
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
+        
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -46,5 +62,8 @@ function gotPoses(results) {
       console.log("Pose Net has been intialized");
   }
  
+  function play() {
+      song_1.play();
+  }
 
 
